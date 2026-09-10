9793277b0041b11f04e6abd605c08a7ebf8d8651
Amzzzzzzzz <ameerarashid04@gmail.com>
2026-09-10 19:48:12 +0100

    Improve responsive navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,69 @@
 import "./Navbar.css";
-import "./Navbar.css";
 import logo from "../../assets/images/logo.jpeg";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="navbar">
 
-<div className="logo">
-    <img src={logo} alt="Space Planning Design Consulting Logo" />
-</div>
+      <div className="logo">
+        <img
+          src={logo}
+          alt="Space Planning Design Consulting Logo"
+        />
+      </div>
 
-      <nav>
+      <nav className={menuOpen ? "mobileMenu active" : "mobileMenu"}>
 
         <ul className="navLinks">
 
-          <li><a href="#about">About</a></li>
+          <li>
+            <a href="#about" onClick={() => setMenuOpen(false)}>
+              About
+            </a>
+          </li>
 
-          <li><a href="#services">Services</a></li>
+          <li>
+            <a href="#services" onClick={() => setMenuOpen(false)}>
+              Services
+            </a>
+          </li>
 
-          <li><a href="#packages">Packages</a></li>
+          <li>
+            <a href="#packages" onClick={() => setMenuOpen(false)}>
+              Packages
+            </a>
+          </li>
 
-          <li><a href="#experience">Experience</a></li>
+          <li>
+            <a href="#experience" onClick={() => setMenuOpen(false)}>
+              Industries
+            </a>
+          </li>
 
-          <li><a href="#contact">Contact</a></li>
+          <li>
+            <a href="#contact" onClick={() => setMenuOpen(false)}>
+              Contact
+            </a>
+          </li>
 
         </ul>
 
+<a href="#contact" className="consultButton">
+    Book Consultation
+</a>
+
       </nav>
 
-      <button>
-        Book Consultation
-      </button>
+      <div
+        className="menuIcon"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
 
     </header>
   );
